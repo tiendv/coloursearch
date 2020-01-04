c90940c42fe08570dd4d714728fb73da62adc8b3
Nguyen Hao <nxhao235@gmail.com>
2020-01-04 21:29:51 +0700

    Update performance evaluation

--- src/cbir/management/commands/evaluate.py
+++ src/cbir/management/commands/evaluate.py
@@ -9,7 +9,11 @@ class Command(BaseCommand):
         parser.add_argument('database_name', type=str)
         parser.add_argument('query_path', type=str)
         parser.add_argument('extraction_id', type=float)
+        parser.add_argument('k', type=int)
 
     def handle(self, *args, **options):
-        status = evaluate_performance(options['database_name'], options['query_path'], options['extraction_id'])
+        status = evaluate_performance(options['database_name'],
+                                      options['query_path'],
+                                      options['extraction_id'], 
+                                      options['k'])
         self.stdout.write(self.style.SUCCESS('Command done.'))
